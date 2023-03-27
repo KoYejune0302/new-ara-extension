0092ff32af90e2cb12cf3dc7065c3b6d39e29bab
Yejune Ko <koyejune0302@gmail.com>
2023-03-27 21:13:10 +0900

    1
    1

--- src/i18n.js
+++ src/i18n.js
@@ -1,26 +1,29 @@
-import { createI18n } from 'vue-i18n'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
 
-/**
- * Load locale messages
- *
- * The loaded `JSON` locale messages is pre-compiled by `@intlify/vue-i18n-loader`, which is integrated into `vue-cli-plugin-i18n`.
- * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
- */
-function loadLocaleMessages() {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+Vue.use(VueI18n)
+
+function loadLocaleMessages () {
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.yaml$/i)
   const messages = {}
   locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+    const matched = key.match(/([a-z0-9]+)\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
-      messages[locale] = locales(key).default
+      messages[locale] = locales(key)
     }
   })
   return messages
 }
 
-export default createI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+const i18n = new VueI18n({
+  locale: 'ko',
+  fallbackLocale: 'ko',
   messages: loadLocaleMessages()
 })
+
+export const changeLocale = () => {
+  i18n.locale = i18n.locale === 'en' ? 'ko' : 'en'
+}
+
+export default i18n
